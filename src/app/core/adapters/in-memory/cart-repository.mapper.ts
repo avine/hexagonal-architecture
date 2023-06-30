74fdfee02eebe180e5b68ff047cd9fc5fe913e87
Stéphane Francel <stephane.francel@gmail.com>
2023-06-30 07:02:49 +0200

    Minor changes

--- src/app/core/adapters/in-memory/cart-repository.mapper.ts
+++ src/app/core/adapters/in-memory/cart-repository.mapper.ts
@@ -2,10 +2,10 @@ import { CartItem } from '@domain';
 
 import { CartItemDto } from './data.types';
 
+// --- Response mappers ---
+
 export const mapToCartItem = ({ id, desc: title, price }: CartItemDto): CartItem => ({
   productId: Number(id),
   title,
   price,
 });
-
-export const mapToCartItemDtoId = (id: number) => String(id);
--- src/app/core/adapters/in-memory/catalog-repository.mapper.ts
+++ src/app/core/adapters/in-memory/catalog-repository.mapper.ts
@@ -2,11 +2,15 @@ import { Product } from '@domain';
 
 import { ProductDto } from './data.types';
 
+// --- Response mappers ---
+
 export const mapToProduct = ({ id, title, price, availableUnits: stock }: ProductDto): Product => ({
   id: Number(id),
   title,
   price,
   stock,
 });
+
+// --- Request mappers ---
 
 export const mapToProductDtoId = (id: number) => String(id);
